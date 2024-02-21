794cd52d99fe466fa2543f135a57c520817f05b1
Adrian Wilker <56568057+adrianwilker@users.noreply.github.com>
2024-02-21 14:23:45 +0000

    GET /items criado

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!')
+  res.send('Hello, World!!!')
 })
 
 app.get('/oi', function(req, res) {
     res.send('Olá, mundo!')
+})
+
+const list = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+app.get('/items', function(req, res) {
+  res.send(list)
 })
 
 app.listen(3000)
